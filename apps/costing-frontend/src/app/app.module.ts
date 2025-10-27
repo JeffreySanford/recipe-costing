ecb40a84a216941b7e447495c9290e45d6d5036f
jeffrey_sanford@hotmail.com <jeffrey_sanford@hotmail.com>
2025-10-27 11:18:35 -0500

    feat: enhance UI components and styles for improved user experience

--- apps/costing-frontend/src/app/app.module.ts
+++ apps/costing-frontend/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 import { UiMaterialModule } from '@recipe-costing/ui-material';
+import { HttpClientModule } from '@angular/common/http';
 import { App } from './app';
 
 @NgModule({
   declarations: [App],
   imports: [
     BrowserModule,
-    RouterModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
     UiMaterialModule,
     // Add feature modules here
   ],
+  // No manual providers needed for ActivatedRoute
   bootstrap: [App],
 })
 export class AppModule {}
